ed36ef7c8553bb3f3420f386c577a6650781d291
daomu <kenny.lius@alibaba-inc.com>
2018-06-07 14:15:15 +0800

    init

---
+++ src/main/java/com/lyc0/lyc/LycApplication.java
@@ -0,0 +1,12 @@
+package com.lyc0.lyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LycApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lyc0/lyc/LycApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lyc0.lyc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LycApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
