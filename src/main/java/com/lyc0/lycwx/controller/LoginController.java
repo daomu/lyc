3cf5cec1f226bf304146c54d2c09b74159056909
daomu <kenny.lius@alibaba-inc.com>
2018-10-31 22:25:50 +0800

    commit

--- src/main/java/com/lyc0/lycwx/controller/LoginController.java
+++ src/main/java/com/lyc0/lycwx/controller/LoginController.java
@@ -1,14 +1,21 @@
 package com.lyc0.lycwx.controller;
 
+import com.lyc0.lycwx.manager.LoginManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/wx")
 public class LoginController {
 
+    @Resource
+    private LoginManager loginManager;
+
     @RequestMapping("/login")
     public String login(String code) {
+        loginManager.login(code);
         return "login success:"+code;
     }
 }
